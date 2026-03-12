4369dea4b0bd4b472b53a773e57435bb8a6e1648
emasterdrs <emasterdrs@users.noreply.github.com>
2026-03-12 22:24:30 +0900

    fix: update base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sales-dashboard/',
+  base: '/sales-dashboard-new/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
